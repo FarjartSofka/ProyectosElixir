c6b22844d9a0ac5af8691f91d55f152b8849b2b9
Farly Artunduaga <farly.artunduaga@sofka.com.co>
2022-10-25 07:35:42 -0500

    integracion nueva funcionalidad delete

--- minimal_todo.ex
+++ minimal_todo.ex
@@ -2,6 +2,52 @@ defmodule MinimalTodo do
   def start do
     filename = IO.gets("Nombre del archivo a cargar: ") |> String.trim
     read(filename)
+      |> parse
+      |> get_command
+  end
+
+  def show_todos(data, next_command? \\ true) do
+    items = Map.keys data
+    IO.puts "Tienes los siguientes items por hacer: \n"
+    Enum.each items, fn item -> IO.puts item end
+    IO.puts "\n"
+    if next_command? do
+      get_command(data)
+    end
+  end
+
+  def get_command(data) do
+    prompt = """
+    Escribe la letra del comando que quieres usar \n
+    (L)eer To-dos (A)ñadir nuevo (B)orrar (C)argar archivo (G)rabar archivo (X) Salir
+    """
+    command = IO.gets(prompt)
+      |> String.trim
+      |> String.downcase
+
+    case command doa
+      "l" ->   show_todos(data)
+      #"a" ->
+      "b" ->   delete_todo(data)
+      #"c" ->
+      #"g" ->
+      "x" -> "Adios!!"
+      _ -> get_command(data)
+    end
+  end
+
+  def delete_todo(data) do
+    todo = IO.gets("Cual tarea deseas borrar?\n") |> String.trim
+    if Map.has_key? data, todo do
+      IO.puts "Se borrara la tareas #{todo}"
+      new_map = Map.drop(data, [todo])
+      IO.puts ~s("#{todo} sera eliminado")
+      get_command(new_map)
+    else
+      IO.puts ~s(No existe la tarea "#{todo}")
+      show_todos(data, false)
+      delete_todo(data)
+    end
   end
 
   def read(filename) do
@@ -14,6 +60,21 @@ defmodule MinimalTodo do
   end
 
   def parse(body) do
-    lines = String.split(body, ~r{(\r\n|\r|\n)})
+    [header | lines] = String.split(body, ~r{(\r\n|\r|\n)})
+    IO.puts([header | lines])
+    titles = tl String.split(header, ";")
+    parse_lines(lines, titles)
+  end
+
+  def parse_lines(lines, titles) do
+    Enum.reduce(lines, %{}, fn line, built ->
+      [name | fields] = String.split(line, ";")
+      if Enum.count(fields) == Enum.count(titles) do
+        line_data = Enum.zip(titles, fields) |> Enum.into(%{})
+        Map.merge(built, %{name => line_data})
+      else
+        built
+      end
+    end)
   end
 end
